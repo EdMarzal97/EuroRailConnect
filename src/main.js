6671c1c8c18d30d08452cf4fedbe6b8268c615c0
Edward Marzal <edsanchezmarzal@gmail.com>
2023-05-21 09:06:29 -0500

    adding some languages

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import i18n from '@/plugins/i18n'
-
+import { i18n } from './plugins/i18n'
 import App from './App.vue'
 import router from './router'
 
@@ -12,4 +11,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(i18n)
 app.mount('#app')
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,12 +1,22 @@
+/* eslint-disable no-unused-vars */
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
 const messages = {
-  en: {},
-  es: {},
-  de: {}
+  en: {
+    welcometitle: 'Welcome',
+    subtitle: 'where are you today?'
+  },
+  es: {
+    welcometitle: 'Bienvenido',
+    subtitle: 'En donde te encuentras hoy?'
+  },
+  de: {
+    welcometitle: 'Willkommen',
+    subtitle: 'Wo bist du heute?'
+  }
 }
 
 const i18n = new VueI18n({
